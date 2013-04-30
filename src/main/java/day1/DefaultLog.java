8699d653a8f748fae5260f60f61c46da896ecfc2
bungubbang <bungubbang57@gmail.com>
2013-04-30 18:24:01 +0900

    maven init commit

---
+++ src/main/java/day1/DefaultLog.java
@@ -0,0 +1,12 @@
+package day1;
+
+/**
+ * Created by bungubbang
+ * Email: bungubbang57@gmail.com
+ */
+public class DefaultLog implements Log {
+    @Override
+    public void write(String data) {
+        System.out.println("data = [" + data + "]");
+    }
+}
---
+++ src/main/java/day1/Log.java
@@ -0,0 +1,9 @@
+package day1;
+
+/**
+ * Created by bungubbang
+ * Email: bungubbang57@gmail.com
+ */
+public interface Log {
+    void write(String data);
+}
