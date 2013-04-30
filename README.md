f050a88b97842a2129f23b3c7010b58eedafcd04
bungubbang <bungubbang57@gmail.com>
2013-04-30 18:27:43 +0900

    merge maven project

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ spring_study
 * Day1
     - Main - Client - Log 를 잊는 프로그램 제작
     - SOILD원칙
+    - maven project로 변경
--- src/Log.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by bungubbang
- * Email: bungubbang57@gmail.com
- */
-public interface Log {
-    void write(String data);
-}
